3d3b529588df14ce2d5999f0bbfd1ad0817f2a65
vgri <vgri199712@163.com>
2022-06-20 14:46:43 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary"> Tertiary </n-button>
+    <n-button type="primary"> Primary </n-button>
+    <n-button type="info"> Info </n-button>
+    <n-button type="success"> Success </n-button>
+    <n-button type="warning"> Warning </n-button>
+    <n-button type="error"> Error </n-button>
+  </n-space>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
